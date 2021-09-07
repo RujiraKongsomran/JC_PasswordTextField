45815bfdf86d65a1ccb8a8bd20321c040190198c
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-07 15:05:53 +0700

    Password visibility icon

--- app/src/main/java/com/rujirakongsomran/jc_passwordtextfield/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_passwordtextfield/MainActivity.kt
@@ -6,18 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 import com.rujirakongsomran.jc_passwordtextfield.ui.theme.JC_PasswordTextFieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,13 +38,30 @@ fun Greeting() {
         verticalArrangement = Arrangement.Center
     ) {
         var password by rememberSaveable { mutableStateOf("") }
+        var passwordVisibility by remember { mutableStateOf(false) }
+
+        val icon = if (passwordVisibility)
+            painterResource(id = R.drawable.design_ic_visibility)
+        else
+            painterResource(id = R.drawable.design_ic_visibility_off)
+
         OutlinedTextField(
             value = password,
             onValueChange = {
                 password = it
             },
             placeholder = { Text(text = "Password") },
-            label = { Text(text = "Password")}
+            label = { Text(text = "Password") },
+            trailingIcon = {
+                IconButton(onClick = {
+                    passwordVisibility = !passwordVisibility
+                }) {
+                    Icon(
+                        painter = icon,
+                        contentDescription = "Visibility Icon"
+                    )
+                }
+            }
         )
     }
 }
