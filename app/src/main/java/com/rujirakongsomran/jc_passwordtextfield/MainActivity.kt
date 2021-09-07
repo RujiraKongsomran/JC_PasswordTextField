2cea40f23dc4cddf8c4ca5189903965bef9b731c
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-07 14:46:30 +0700

    Add OutlinedTextField and Text State

--- app/src/main/java/com/rujirakongsomran/jc_passwordtextfield/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_passwordtextfield/MainActivity.kt
@@ -3,11 +3,20 @@ package com.rujirakongsomran.jc_passwordtextfield
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.rujirakongsomran.jc_passwordtextfield.ui.theme.JC_PasswordTextFieldTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,7 @@ class MainActivity : ComponentActivity() {
             JC_PasswordTextFieldTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -25,14 +34,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var password by rememberSaveable { mutableStateOf("") }
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JC_PasswordTextFieldTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
